fb172f4bafd093d775fb4caad0b847c82ea422b1
shivanisingh5046976@gmail.com <shivanisingh5046976@gmail.com>
2024-07-20 12:38:04 +0530

    Basic React

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,12 @@
+import Learn from "./Learn"
+
+function App() {
+  return (
+    <>
+    <Learn />
+    {/* <h1>Learning React | Shivani Singh</h1> */}
+    </>
+  );
+}
+
+export default App;
---
+++ 01basicreact/src/Learn.jsx
@@ -0,0 +1,7 @@
+function Learn(){
+    return(
+        <h2>Learning React using Create-React-App</h2>
+    )
+}
+
+export default Learn
